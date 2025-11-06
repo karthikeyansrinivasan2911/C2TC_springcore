f1edc355f326cfa224b626d22cda72281b7b8905
karth <karth@karthi>
2025-11-06 15:16:06 +0530

    commit

---
+++ IOC/src/main/java/org/springframework/context/ApplicationContext.java
@@ -0,0 +1,12 @@
+package org.springframework.context;
+
+import com.tnsif.IOC.Mobile;
+
+public class ApplicationContext {
+
+	public Mobile getBean(String string, Class<Mobile> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
